9fd80596560e07e42432feadfece2def95673070
JPJ-GitProfile <jjones0419@gmail.com>
2019-10-29 19:09:07 -0400

    newpages to commit

---
+++ PHPExample1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <p>Testing mother </p>
+        <?php echo '<p>Hi, Im a PHP script</p>'; ?>
+    </body>
+</html>
